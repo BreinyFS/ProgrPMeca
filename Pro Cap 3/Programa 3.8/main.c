e680c8bb4e30bed0b02c8e0ba13476f5ca1e7a64
BreinyFS <133532690+BreinyFS@users.noreply.github.com>
2023-05-29 12:47:26 -0400

    Ejercicios/Ejemplos Cap 3

---
+++ Pro Cap 3/Programa 3.8/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*Factorial.
+FAC, I, NUM: variables de tipo entero. */
+
+int main()
+{
+  int I, NUM;
+  long FAC;
+  printf("\nIngrese el numero: ");
+  scanf("%d", &NUM);
+  if (NUM >= 0)
+    {
+    FAC = 1;
+    for(I=1; I <= NUM; I++)
+      FAC *= I;
+    printf("\nEl factorial de %d es: %Id", NUM, FAC);
+    }
+  else
+  printf("\nError en el dato ingresado");
+}
---
+++ Pro Cap 3/Programa 3.9/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/* Serie.
+I, SSE Y CAM: variables de tipo entero. */
+
+int main()
+{
+  int I= 2, CAM =1;
+  long SSE = 0;
+  while (I <= 2500)
+    {
+      SSE = SSE + I;
+      printf("\t %d", I);
+      if (CAM)
+      {
+        I += 5;
+        CAM--;
+      }
+      else
+      {
+        I += 3;
+        CAM++;
+      }
+    }
+  printf("\nLa suma de la serie es: %Id", SSE);
+}
