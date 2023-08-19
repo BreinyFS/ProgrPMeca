43f71cf9bce53e137662895b474615f928afa299
BreinyFS <133532690+BreinyFS@users.noreply.github.com>
2023-08-19 02:19:07 +0000

    Programa de Emergencia Arreglado 18/08/2023

---
+++ Programa de Emergencia/Programa de Emergencia/UserControl1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Programa_de_Emergencia
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNext = new System.Windows.Forms.Button();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.cmbAnswer = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnNext
+            // 
+            this.btnNext.BackColor = System.Drawing.Color.Purple;
+            this.btnNext.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.btnNext.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnNext.Location = new System.Drawing.Point(3, 34);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(794, 45);
+            this.btnNext.TabIndex = 0;
+            this.btnNext.Text = "Inicio";
+            this.btnNext.UseVisualStyleBackColor = false;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.lblQuestion.Location = new System.Drawing.Point(28, 118);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(58, 22);
+            this.lblQuestion.TabIndex = 2;
+            this.lblQuestion.Text = "label1";
+            // 
+            // cmbAnswer
+            // 
+            this.cmbAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.cmbAnswer.FormattingEnabled = true;
+            this.cmbAnswer.Location = new System.Drawing.Point(32, 177);
+            this.cmbAnswer.Name = "cmbAnswer";
+            this.cmbAnswer.Size = new System.Drawing.Size(262, 28);
+            this.cmbAnswer.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.button2.Location = new System.Drawing.Point(32, 352);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 35);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Siguiente";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.button3.Location = new System.Drawing.Point(632, 352);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 35);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Finalizar ";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(32, 250);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(721, 49);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(437, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtNombre.Location = new System.Drawing.Point(519, 115);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 26);
+            this.txtNombre.TabIndex = 12;
+            this.txtNombre.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.cmbAnswer);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.btnNext);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(800, 450);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.ComboBox cmbAnswer;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombre;
+    }
+}
---
+++ Programa de Emergencia/Programa de Emergencia/UserControl1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Programa_de_Emergencia
+{
+    public partial class UserControl1: UserControl
+    {
+        private int currentQuestion = 0;
+
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void DisplayQuestion(int questionNumber)
+        {
+            if (questionNumber == 0)
+            {
+                lblQuestion.Text = "Edad del paciente";
+                cmbAnswer.Items.Clear();
+                cmbAnswer.Items.AddRange(new string[] { "Menor de 10", "Entre 10-20", "Entre 21-40", "Mayor a 40" });
+                progressBar1.Value = 14;
+            }
+            if (questionNumber == 1)
+            {
+                lblQuestion.Text = "¿Cuál es su nivel de fiebre?";
+                cmbAnswer.Items.Clear();
+                cmbAnswer.Items.AddRange(new string[] { "Muy Alto", "Alto", "Normal" });
+                progressBar1.Value = 28;
+            }
+            else if (questionNumber == 2)
+            {
+                lblQuestion.Text = "¿Tienes dificultad para respirar?";
+                cmbAnswer.Items.Clear();
+                cmbAnswer.Items.AddRange(new string[] { "Sí", "No siempre", "No" });
+                progressBar1.Value = 42;
+            }
+            else if (questionNumber == 3)
+            {
+                lblQuestion.Text = "¿Tienes dolor muscular?";
+                cmbAnswer.Items.Clear();
+                cmbAnswer.Items.AddRange(new string[] { "Sí", "No" });
+                progressBar1.Value = 56;
+            }
+            else if (questionNumber == 4)
+            {
+                lblQuestion.Text = "¿Sientes mareos y/o nauceas?";
+                cmbAnswer.Items.Clear();
+                cmbAnswer.Items.AddRange(new string[] { "Sí", "No" });
+                progressBar1.Value = 70;
+            }
+            else if (questionNumber == 5)
+            {
+                lblQuestion.Text = "¿Se le dificulta hablar?";
+                cmbAnswer.Items.Clear();
+                cmbAnswer.Items.AddRange(new string[] { "Sí", "No" });
+                btnNext.Text = "Evaluar";
+                progressBar1.Value = 84;
+            }
+            else if (questionNumber == 6)
+            {
+                EvaluateEmergency();
+                progressBar1.Value = 100;
+            }
+        }
+
+
+        private void btnNext_Click_1(object sender, EventArgs e)
+        {
+            string selectedAnswer = cmbAnswer.SelectedItem as string;
+
+            if (currentQuestion <= 5)
+            {
+                ProcessAnswer(currentQuestion, selectedAnswer);
+                currentQuestion++;
+                DisplayQuestion(currentQuestion);
+            }
+        }
+        //  para cada respuesta individualmente
+        private void ProcessAnswer(int questionNumber, string answer)
+        {
+            if (questionNumber == 1)
+            {
+                if (answer == "Normal")
+                {
+                    MessageBox.Show("Mantente hidratado junto a un buen descanso.");
+                }
+                else if (answer == "Alto")
+                {
+                    MessageBox.Show("Toma medicamentos para bajar la fiebre y reposa");
+                }
+                else if (answer == "Muy Alto")
+                {
+                    MessageBox.Show("Acude a atención médica de inmediato.");
+                }
+            }
+            else if (questionNumber == 2)
+            {
+                if (answer == "Sí")
+                {
+                    MessageBox.Show("Busca atención médica de inmediato y haga uso de un ihnalador o respirador.");
+                }
+                if (answer == "No siempre")
+                {
+                    MessageBox.Show(" Haga rehabilitacion pulmunar y terapia fisica.");
+                }
+                else if (answer == "No")
+                {
+                    MessageBox.Show("Descansa y mantente pendiente de tus síntomas.");
+                }
+            }
+            else if (questionNumber == 3)
+            {
+                if (answer == "Sí")
+                {
+                    MessageBox.Show("Aplica crema en el musculo y visita un experto.");
+                }
+                else if (answer == "No")
+                {
+                    MessageBox.Show("Continúa al tanto de tus síntomas.");
+                }
+            }
+            else if (questionNumber == 4)
+            {
+                if (answer == "Sí")
+                {
+                    MessageBox.Show("Descansa, mantente hidratado e interrumpa la dosis de cualquie medicamento.");
+                }
+                else if (answer == "No")
+                {
+                    MessageBox.Show("Continúa monitoreando tus síntomas.");
+                }
+            }
+            else if (questionNumber == 5)
+            {
+                if (answer == "Sí")
+                {
+                    MessageBox.Show("Busca atención médica de inmediato, es posible que te den terapia del habla y el lenguaje.");
+                }
+                else if (answer == "No")
+                {
+                    MessageBox.Show("Descansa y permanece en tranquilidad.");
+                }
+            }
+        }
+
+        private void EvaluateEmergency()
+        {
+            // Realiza la evaluación final de las respuestas y presenta un procedimiento
+            MessageBox.Show("¡Situacion evaluada! Seguir las recomendaciones.");
+            
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            DisplayQuestion(currentQuestion);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string selectedAnswer = cmbAnswer.SelectedItem as string;
+
+            if (currentQuestion <= 5)
+            {
+                ProcessAnswer(currentQuestion, selectedAnswer);
+                currentQuestion++;
+                DisplayQuestion(currentQuestion);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
